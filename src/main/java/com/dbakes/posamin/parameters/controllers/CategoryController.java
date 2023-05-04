d98fa2bd404ebe4c3f583cb3fccfd42c09ada88e
kouatchoua mark <kouatchres@gmail.com>
2023-05-04 05:38:15 +0100

    category add commit

--- src/main/java/com/dbakes/posamin/parameters/controllers/CategoryController.java
+++ src/main/java/com/dbakes/posamin/parameters/controllers/CategoryController.java
@@ -1,13 +1,32 @@
 package com.dbakes.posamin.parameters.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.dbakes.posamin.parameters.models.Category;
+import com.dbakes.posamin.parameters.services.CategoryService;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CategoryController {
+    @Autowired
+public CategoryService categoryService;
 
-   @GetMapping("/categoryList")
-   public String catList(){
-    return "/categoryList";
+    @GetMapping("/categoryAdd")
+    public String addNewCat(){
+        return "/parameters/categoryAdd";
+    }
+
+
+
+   @GetMapping("/categories")
+   public String getAllCategories(Model model){
+        List<Category> categories = categoryService.listOfCategories();
+model.addAttribute("categories", categories);
+   return "/parameters/categoryList";
    }
 }
--- src/main/java/com/dbakes/posamin/parameters/services/CategoryService.java
+++ src/main/java/com/dbakes/posamin/parameters/services/CategoryService.java
@@ -1,8 +1,11 @@
 package com.dbakes.posamin.parameters.services;
 
+import com.dbakes.posamin.parameters.models.Category;
 import com.dbakes.posamin.parameters.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
@@ -10,6 +13,9 @@ public class CategoryService {
 @Autowired
 private CategoryRepository categoryRepository;
 
+public List<Category> listOfCategories(){
+    return categoryRepository.findAll();
+}
 
 
 }
